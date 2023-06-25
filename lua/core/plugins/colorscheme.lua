4551b933f3a7d74bab1004263c6910d238f795cb
Jobin Nelson <jobinnelson369@gmail.com>
2023-06-25 09:32:03 +0530

    add configuration to catppuccin plugin

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -3,6 +3,27 @@ return {
 	lazy = false,
   priority = 1000,
   config = function()
+    require('catppuccin').setup({
+      transparent_background = false,
+      dim_inactive = {
+        enabled = true,
+        shade = 'dark',
+        percentage = 0.15,
+      },
+      integrations = {
+        nvimtree = false,
+        ts_rainbow = false,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        which_key = true,
+      },
+    })
     vim.cmd.colorscheme 'catppuccin'
   end,
 	name = 'catppuccin',
