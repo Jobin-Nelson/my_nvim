bcc48b00f51d1a0ba7619efbafdc094c65979f9b
Jobin Nelson <jobinnelson369@gmail.com>
2023-08-01 11:20:31 +0530

    add git hist alias

--- lua/core/plugins/git.lua
+++ lua/core/plugins/git.lua
@@ -1,13 +1,14 @@
 return {
   'lewis6991/gitsigns.nvim',
   event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-  },
+  opts = {},
+  -- opts = {
+  --   signs = {
+  --     add = { text = '+' },
+  --     change = { text = '~' },
+  --     delete = { text = '_' },
+  --     topdelete = { text = '‾' },
+  --     changedelete = { text = '~' },
+  --   },
+  -- },
 }
