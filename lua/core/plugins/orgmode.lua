5090c05a60ba994dc7ea4921182ff5dc01666d1e
Jobin Nelson <jobinnelson369@gmail.com>
2023-08-23 11:34:23 +0530

    Change font size to 13 and change orgmode file path

--- lua/core/plugins/orgmode.lua
+++ lua/core/plugins/orgmode.lua
@@ -6,7 +6,7 @@ return {
 		require('orgmode').setup({
 			org_agenda_files = {
 				'~/playground/projects/org_files/**/*',
-				'~/playground/dev/illumina/ticket_notes/work/**/*',
+				'~/playground/dev/illumina/ticket_notes/work_org_files/**/*',
 			},
 			org_default_notes_file = '~/playground/projects/org_files/refile.org',
 			org_todo_keywords = { 'TODO(t)', 'NEXT(n)', '|', 'DONE(d)', 'CANCELLED(c)' },
