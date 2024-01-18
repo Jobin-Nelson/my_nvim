410f547b6ee9df0ef7853192e5eb0028f2b70b98
Jobin Nelson <jobinnelson369@gmail.com>
2024-01-18 11:14:30 +0530

    fix: add logic to not select same bufnr in better_bufdelete function in utils.lua

--- lua/jobin/config/custom/utils.lua
+++ lua/jobin/config/custom/utils.lua
@@ -4,7 +4,8 @@
 local M = {}
 
 local function is_valid_buf(buf)
-  return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted
+  return vim.api.nvim_buf_is_loaded(buf)
+  -- return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted
 end
 
 M.delete_hidden_buffers = function()
@@ -189,8 +190,8 @@ function M.better_bufdelete()
   local next_bufnr = nil
 
   ---@diagnostic disable-next-line: param-type-mismatch
-  for nr = 1, vim.fn.bufnr('$') do
-    if is_valid_buf(nr) then
+  for _, nr in ipairs(vim.api.nvim_list_bufs()) do
+    if is_valid_buf(nr) and next_bufnr ~= bufnr then
       next_bufnr = nr
       break
     end
