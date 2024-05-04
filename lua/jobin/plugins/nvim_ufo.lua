aea4e8c19454e56b72ec5280e1f13a98d6c225f3
Jobin Nelson <jobinnelson369@gmail.com>
2024-05-05 00:37:27 +0530

    docs: add description for ufo folding keymap

--- lua/jobin/plugins/nvim_ufo.lua
+++ lua/jobin/plugins/nvim_ufo.lua
@@ -105,10 +105,10 @@ return {
       end
     })
 
-    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
-    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = 'Open all folds' })
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all folds' })
+    vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds, { desc = 'Fold less' })
+    vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith, { desc = 'Fold more' })
     -- vim.keymap.set('n', 'K', function()
     --   local winid = require('ufo').peekFoldedLinesUnderCursor()
     --   if not winid then
