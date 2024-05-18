744d9eaee76fe08c0791bbacc8ad685aadd232ab
Jobin Nelson <jobinnelson369@gmail.com>
2024-05-18 15:48:18 +0530

    fix: disable default codeium bindings

--- lua/jobin/plugins/codeium.lua
+++ lua/jobin/plugins/codeium.lua
@@ -1,10 +1,10 @@
+vim.g.codeium_enabled = false
+vim.g.codeium_disable_bindings = 1
+
 return {
   'Exafunction/codeium.vim',
   event = 'BufEnter',
   config = function()
-    vim.g.codeium_enabled = false
-    vim.g.codeium_disable_bindings = 1
-
     vim.keymap.set('i', '<A-l>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true, desc = 'Codeium Accept' })
     vim.keymap.set('i', '<A-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { desc = 'Codeium Cycle Next' })
     vim.keymap.set('i', '<A-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { desc = 'Codeium Cycle Prev' })
