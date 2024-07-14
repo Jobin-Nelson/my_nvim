8918360291361eac3e7852da430e1af585ae2931
Jobin Nelson <jobinnelson369@gmail.com>
2024-07-14 16:43:56 +0530

    feat: add gen.nvim

--- lua/jobin/plugins/which_key.lua
+++ lua/jobin/plugins/which_key.lua
@@ -1,31 +1,23 @@
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  config = function()
-    local mappings = {
-      f = { name = "Find" },
-      b = { name = "Buffers" },
-      l = { name = "Lsp" },
-      p = { name = "Package" },
-      t = { name = "Terminal" },
-      w = { name = "Work" },
-      j = { name = "Custom" },
-      g = { name = "Git" },
-      d = { name = "Debug" },
-      u = { name = "UI" },
-      o = { name = "Org mode" },
-      s = { name = "Session" },
+  opts = {
+    defaults = {
+      preset = "modern",
+    },
+    spec = {
+      { "<leader>f", group = "Find",     nowait = true, remap = false },
+      { "<leader>b", group = "Buffers",  nowait = true, remap = false },
+      { "<leader>l", group = "Lsp",      nowait = true, remap = false },
+      { "<leader>p", group = "Package",  nowait = true, remap = false },
+      { "<leader>t", group = "Terminal", nowait = true, remap = false },
+      { "<leader>w", group = "Work",     nowait = true, remap = false },
+      { "<leader>j", group = "Custom",   nowait = true, remap = false },
+      { "<leader>g", group = "Git",      nowait = true, remap = false },
+      { "<leader>d", group = "Debug",    nowait = true, remap = false },
+      { "<leader>u", group = "UI",       nowait = true, remap = false },
+      { "<leader>o", group = "Org mode", nowait = true, remap = false },
+      { "<leader>s", group = "Session",  nowait = true, remap = false },
     }
-    local opts = {
-      mode = "n",
-      prefix = "<leader>",
-      buffer = nil,
-      silent = true,
-      noremap = true,
-      nowait = true,
-    }
-    local which_key = require('which-key')
-    which_key.setup()
-    which_key.register(mappings, opts)
-  end
+  },
 }
