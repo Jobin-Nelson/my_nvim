37432850160e97f13d815659769b754ffe3d7f76
jobin_nelson <jobinnelson369@gmail.com>
2024-08-01 00:43:33 +0530

    feat: add gruvbox-material

--- lua/jobin/plugins/colorscheme.lua
+++ lua/jobin/plugins/colorscheme.lua
@@ -41,10 +41,10 @@ local integrations = {
 return {
   {
     "catppuccin/nvim",
-    name     = "catppuccin",
+    name = "catppuccin",
     -- lazy     = false,
     -- priority = 1000,
-    config   = function()
+    config = function()
       require('catppuccin').setup({
         transparent_background = true,
         dim_inactive = {
@@ -78,6 +78,18 @@ return {
     end,
   },
   {
-    "slugbyte/lackluster.nvim",
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = 'hard'
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_enable_bold = true
+      -- vim.g.gruvbox_material_transparent_background = 0
+      vim.g.gruvbox_material_dim_inactive_windows = 1
+      vim.g.gruvbox_material_float_style = 'dim'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.cmd.colorscheme('gruvbox-material')
+    end
   }
 }
