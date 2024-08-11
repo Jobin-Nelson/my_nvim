362da7abc2d5146ac4c951d02af900ab3b06dc77
jobin_nelson <jobinnelson369@gmail.com>
2024-08-11 23:09:40 +0530

    feat: add back gruvbox-material theme

--- lua/jobin/plugins/lsp.lua
+++ lua/jobin/plugins/lsp.lua
@@ -4,20 +4,21 @@ return {
   dependencies = {
     { 'williamboman/mason.nvim', cmd = 'Mason', opts = { ui = { border = 'rounded' } } },
     'williamboman/mason-lspconfig.nvim',
+    'b0o/schemastore.nvim',
     {
       'j-hui/fidget.nvim',
       opts = {
         progress = {
           display = {
-            render_limit = 16,     -- How many LSP messages to show at once
-            done_ttl = 3,          -- How long a message should persist after completion
+            render_limit = 16, -- How many LSP messages to show at once
+            done_ttl = 3,      -- How long a message should persist after completion
             done_icon = '✔',
             progress_icon = { pattern = 'meter', period = 1 },
           },
         },
 
         notification = {
-          poll_rate = 60,   -- FPS
+          poll_rate = 60, -- FPS
           view = {
             stack_upwards = true,
             icon_separator = " ",
@@ -25,9 +26,9 @@ return {
           },
 
           window = {
-            normal_hl = "Comment",     -- Base highlight group in the notification window
-            winblend = 0,              -- Background color opacity in the notification window
-            border = "single",         -- Border around the notification window
+            normal_hl = "Comment", -- Base highlight group in the notification window
+            winblend = 0,          -- Background color opacity in the notification window
+            border = "single",     -- Border around the notification window
             x_padding = 1,
             y_padding = 0,
             relative = "editor",
--- lua/jobin/plugins/others.lua
+++ lua/jobin/plugins/others.lua
@@ -1,11 +1,10 @@
 return {
   { "dhruvasagar/vim-table-mode", keys = { "<leader>tm" } },
   { "dhruvasagar/vim-zoom",       keys = { "<C-w>m" } },
-  { "windwp/nvim-autopairs",      event = "InsertEnter",                   config = true },
-  { "kylechui/nvim-surround",     event = { "BufReadPost", "BufNewFile" }, config = true },
-  { 'b0o/schemastore.nvim' },
-  { 'stevearc/dressing.nvim',     event = 'VeryLazy',                      config = true },
-  { 'akinsho/toggleterm.nvim',    cmd = 'ToggleTerm',                      config = true },
-  { 'folke/neodev.nvim',          ft = 'lua' , config = true },
-  { 'sindrets/diffview.nvim',     cmd = 'DiffviewFileHistory' , config = true },
+  { "windwp/nvim-autopairs",      event = "InsertEnter",                           config = true },
+  { "kylechui/nvim-surround",     event = { "BufReadPost", "BufNewFile" },         config = true },
+  { 'stevearc/dressing.nvim',     event = 'VeryLazy',                              config = true },
+  { 'akinsho/toggleterm.nvim',    cmd = 'ToggleTerm',                              config = true },
+  { 'folke/neodev.nvim',          ft = 'lua',                                      config = true },
+  { 'sindrets/diffview.nvim',     cmd = { 'DiffviewFileHistory', 'DiffViewOpen' }, config = true },
 }
