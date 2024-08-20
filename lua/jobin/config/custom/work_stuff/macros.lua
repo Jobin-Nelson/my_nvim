355c69bf5273824b0f00ea877efd6853a8127b6f
jnelson4 <jnelson4@illumina.com>
2024-08-20 22:44:17 +0530

    feat: add macro for converting org link to markdown link

--- lua/jobin/config/custom/work_stuff/macros.lua
+++ lua/jobin/config/custom/work_stuff/macros.lua
@@ -1,5 +1,6 @@
 -- macros
-vim.fn.setreg('j', [[0f[x;"adi[h3xF["apa|j]])
+vim.fn.setreg('j', [[0f[x;"adi[h3xF["apa|j]]) -- convert org link to jira link
+vim.fn.setreg('m', [[$hhdi[Bi[P](l2xf]C)j]]) -- convert org link to markdown link
 
 -- mappings
 vim.keymap.set('n', 'yl', '0$hhyi[0', { desc = 'Copy Test-Case ID', silent = true })
