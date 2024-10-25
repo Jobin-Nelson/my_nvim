efa519d17dc6e14d8b42fedf41d7e450e1fcb725
jobin_nelson <jobinnelson369@gmail.com>
2024-10-26 00:56:07 +0530

    feat: add mappings for sending lines to terminal in toggleterm
    This helps to replace the need for jupyter notebook for small scripts

--- lua/jobin/plugins/toggleterm.lua
+++ lua/jobin/plugins/toggleterm.lua
@@ -1,15 +1,23 @@
 return {
-	'akinsho/toggleterm.nvim',
+  'akinsho/toggleterm.nvim',
   keys = {
     '<A-h>',
     '<A-v>',
   },
-	config = function()
+  config = function()
     vim.keymap.set('n', '<A-h>', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', { desc = 'ToggleTerm Horizontal' })
     vim.keymap.set('n', '<A-v>', '<cmd>ToggleTerm size=80 direction=vertical<cr>', { desc = 'ToggleTerm Vertical' })
     vim.keymap.set('t', '<A-h>', '<esc><cmd>ToggleTerm direction=horizontal<cr>', { desc = 'ToggleTerm Horizontal' })
     vim.keymap.set('t', '<A-v>', '<esc><cmd>ToggleTerm direction=vertical<cr>', { desc = 'ToggleTerm Vertical' })
     vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>', { desc = 'Terminal window command' })
+
+    local trim_spaces = false
+    vim.keymap.set({ 'i', 'n' }, '<A-s>', function()
+      require('toggleterm').send_lines_to_terminal('single_line', trim_spaces, { args = vim.v.count })
+    end)
+    vim.keymap.set('v', '<A-s>', function()
+      require('toggleterm').send_lines_to_terminal('visual_lines', trim_spaces, { args = vim.v.count })
+    end)
     require('toggleterm').setup()
   end,
 }
