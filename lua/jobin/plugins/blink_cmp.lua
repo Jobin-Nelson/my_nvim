0cb122c8553f15a25bb20af4597907987c024452
jobin_nelson <jobinnelson369@gmail.com>
2024-12-15 12:21:57 +0530

    feat(blink): add luasnip to blink cmp

--- lua/jobin/plugins/blink_cmp.lua
+++ lua/jobin/plugins/blink_cmp.lua
@@ -1,8 +1,8 @@
 return {
   'saghen/blink.cmp',
-  lazy = false, -- lazy loading handled internally
+  event = 'InsertEnter',
   -- optional: provides snippets for the snippet source
-  dependencies = 'rafamadriz/friendly-snippets',
+  dependencies = { 'L3MON4D3/LuaSnip' },
   -- use a release tag to download pre-built binaries
   version = 'v0.*',
   -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
@@ -14,6 +14,16 @@ return {
   ---@type blink.cmp.Config
   ---@diagnostic disable: missing-fields
   opts = {
+    snippets = {
+      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+      active = function(filter)
+        if filter and filter.direction then
+          return require('luasnip').jumpable(filter.direction)
+        end
+        return require('luasnip').in_snippet()
+      end,
+      jump = function(direction) require('luasnip').jump(direction) end,
+    },
     -- 'default' for mappings similar to built-in completion
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
@@ -50,20 +60,30 @@ return {
         dadbod = {
           name = 'Dadbod',
           module = 'vim_dadbod_completion.blink',
-        }
+        },
+        orgmode = {
+          name = 'Orgmode',
+          module = 'orgmode.org.autocompletion.blink',
+        },
       },
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      default = { 'luasnip', 'lsp', 'path', 'buffer', },
       per_filetype = {
-        sql = { 'buffer', 'dadbod' },
+        sql = { 'luasnip', 'dadbod', 'buffer', },
+        org = { 'luasnip', 'orgmode', 'buffer', },
       },
       -- optionally disable cmdline completions
       -- cmdline = {},
     },
 
     -- experimental signature help support
-    signature = { enabled = true }
+    signature = {
+      enabled = false,
+      window = {
+        border = 'single',
+      },
+    },
   },
   -- allows extending the providers array elsewhere in your config
   -- without having to redefine it
-  opts_extend = { "sources.default" }
+  opts_extend = { "sources.default" },
 }
