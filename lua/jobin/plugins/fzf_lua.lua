6c2876ae1cfad5cb3e8244571dd4ca479840e86f
jobin_nelson <jobinnelson369@gmail.com>
2024-12-31 23:54:53 +0530

    fix(fzf): do not automatically resize ui select window

--- lua/jobin/plugins/fzf_lua.lua
+++ lua/jobin/plugins/fzf_lua.lua
@@ -122,16 +122,7 @@ return {
     })
 
     -- use fzf-lua for vim.ui.select
-    require("fzf-lua").register_ui_select(function(_, items)
-      local min_h, max_h = 0.15, 0.70
-      local h = (#items + 4) / vim.o.lines
-      if h < min_h then
-        h = min_h
-      elseif h > max_h then
-        h = max_h
-      end
-      return { winopts = { height = h, width = 0.50, row = 0.40 } }
-    end)
+    require("fzf-lua").register_ui_select()
 
     -- Custom User commands
     vim.api.nvim_create_user_command('JQL',
