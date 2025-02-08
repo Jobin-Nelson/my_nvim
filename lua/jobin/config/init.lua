cb9eab79412ec95df5c0e021cee5012bff576389
jobin_nelson <jobinnelson369@gmail.com>
2025-02-08 20:15:05 +0530

    refactor(commands): move commands file

--- lua/jobin/config/init.lua
+++ lua/jobin/config/init.lua
@@ -1,7 +1,7 @@
 require('jobin.config.options')
 require('jobin.config.mappings')
+require('jobin.config.commands')
 -- require('jobin.config.statusline')
-require('jobin.config.custom.commands')
 
 -- work
 require('jobin.config.custom.work_stuff.macros')
