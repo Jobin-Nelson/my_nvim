2187c23d551407888f8e7c133da6e92f2a0875d2
jobin_nelson <jobinnelson369@gmail.com>
2025-05-20 16:11:54 +0530

    feat: add bigfile filetype to disable features

--- lua/jobin/config/autocmds.lua
+++ lua/jobin/config/autocmds.lua
@@ -16,39 +16,16 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 })
 
 
--- vim.filetype.add({
---   pattern = {
---     ['.*'] = {
---       function(path, buf)
---         local bigfile_size = 1.5 * 1024 * 1024 -- 1.5MB
---         return vim.bo[buf]
---             and vim.bo[buf].filetype ~= 'bigfile'
---             and path
---             and vim.fn.getfsize(path) > bigfile_size
---             and 'bigfile'
---             or nil
---       end
---     }
---   }
--- })
--- vim.api.nvim_create_autocmd({ 'FileType' }, {
---   group = vim.api.nvim_create_augroup('bigfile', { clear = true }),
---   pattern = 'bigfile',
---   callback = function(ev)
---     vim.api.nvim_buf_call(ev.buf, function()
---       vim.cmd('NoMatchParen')
---       vim.cmd('UfoDetach')
---       local local_opts = {
---         foldmethod = 'manual',
---         statuscolumn = '',
---         conceallevel = 0,
---       }
---       for k, v in pairs(local_opts) do
---         vim.api.nvim_set_option_value(k, v, { scope = 'local', win = ev.win })
---       end
---       vim.schedule(function()
---         vim.bo[ev.buf].syntax = vim.filetype.match({ buf = ev.buf }) or ''
---       end)
---     end)
---   end
--- })
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = vim.api.nvim_create_augroup('jobin/bigfile', { clear = true }),
+  desc = 'Disable features in big files',
+  pattern = 'bigfile',
+  callback = function(ev)
+    vim.api.nvim_buf_call(ev.buf, function()
+      vim.schedule(function()
+        vim.bo[ev.buf].syntax = vim.filetype.match({ buf = ev.buf }) or ''
+        vim.wo[0][0].foldmethod = 'indent'
+      end)
+    end)
+  end
+})
