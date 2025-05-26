bd28239c70a2ea06900ecd526c201d11411baa3d
jobin_nelson <jobinnelson369@gmail.com>
2025-05-26 13:00:21 +0530

    fix: do not set foldexpr to vim.lsp.foldexpr

--- lua/jobin/plugins/lsp.lua
+++ lua/jobin/plugins/lsp.lua
@@ -80,10 +80,10 @@ return {
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if not client then return end
 
-        if client:supports_method('textDocument/foldingRange') then
-          local win = vim.api.nvim_get_current_win()
-          vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
-        end
+        -- if client:supports_method('textDocument/foldingRange') then
+        --   local win = vim.api.nvim_get_current_win()
+        --   vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
+        -- end
 
         if client:supports_method('textDocument/codeLens') then
           vim.lsp.codelens.refresh()
